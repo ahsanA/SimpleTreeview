9e001809023c3b349cea72a265657ccb4ec6ad5a
ahsan <ahsan@isoftgeeks.com>
2015-05-12 02:45:26 +0600

    setup

---
+++ MyCustomTreeView/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MyCustomTreeView.Models;
+
+namespace MyCustomTreeView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Simple()
+        {
+            List<Menu> all = new List<Menu>();
+            using (var dc = new MyMenuContext())
+            {
+                all = dc.Menus.OrderBy(i=>i.ParentId).ToList(); //Menu.OrderBy(a => a.ParentMenuID).ToList();
+            }
+            return View(all);
+        }
+    }
+}
